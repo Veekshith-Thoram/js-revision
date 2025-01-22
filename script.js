f3025901f947a3a43180cb1d48dd940f58326186
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2025-01-21 23:13:06 -0600

    wired input and output and added fetch call from server to render output

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputText = document.querySelector("#output-text");
 
-var txtValue = txtInput.value;
+var serverUrl = "https://api.funtranslaions.com/translate/minion.json";
+
+
+const getTranslationUrl = (text) => {
+    return serverUrl + "?text=" + text; 
+}
+
+const errorHandler = error => {
+    outputText.innerText = "The server is down, you can try after some time"
+}
+
+
 
 btnTranslate.addEventListener("click", () => {
-    console.log(txtInput.value);
+    var inputText = txtInput.value;
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => outputText.innerText = (json.contents.translated))
+        .catch(errorHandler)
 })
